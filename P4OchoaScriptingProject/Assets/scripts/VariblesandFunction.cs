973b6d6c60ba60fa17d15025a92aeae925f597e4
JimmyOchoa815 <768328@student.dusd.net>
2025-11-19 11:49:03 -0800

    Added Scripts
    Added If Statement Scripts

--- P4OchoaScriptingProject/Assets/scripts/VariblesandFunction.cs
+++ P4OchoaScriptingProject/Assets/scripts/VariblesandFunction.cs
@@ -1,37 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    //create my interger variable
-     public int myint = 5;
+      int myint = 5;
 
-    // Start is called before the first frame update
     void Start()
     {
-        //run my variable thru my function
-        myint = MutiplyByTwo(myint);
+       myint =  MultiplyByTwo(myint);
         Debug.Log(myint);
     }
 
-    // Update is called once per frame
-    void Update()
+        
+    int MultiplyByTwo(int number)
     {
-
-    }
-    //create the mutiply by two functions
-    int MutiplyByTwo(int number)
-    {
-        //create local variable
+        
         int result;
-
-        //multiplying the num,ber by two
         result = number * 2;
-
-        //return the value
         return result;
-
     }
-
 }
